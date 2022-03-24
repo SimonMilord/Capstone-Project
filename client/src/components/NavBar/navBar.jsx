bf1d1d262bb554e881b6a9eee84f63a352fc256f
Simon Milord <simon.milord@gmail.com>
2022-03-23 21:37:25 -0400

    added basic styling and responsiveness for navbar

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -9,12 +9,12 @@ export default function NavBar(props) {
   return (
     <div className='navBar'>
       <div className='logo'>
-        <Link to="/"><img className='logo__img' src={Logo} alt="stonkers logo"></img></Link>
+        <Link to="/" className='logo__link'><img className='logo__img' src={Logo} alt="stonkers logo"></img></Link>
         <h1 className='logo__title'>Stonkers</h1>
       </div>
       <div className='search'>
-        <input className="search__input" type="text" name="" placeholder='Search...' />
         <SearchIcon color="action"/>
+        <input className="search__input" type="text" name="" placeholder='Search...' />
       </div>
       <div className='btns'>
         <Link to="/watchlist" className="btns__link btns__link--watchlist" >Watchlist</Link>
