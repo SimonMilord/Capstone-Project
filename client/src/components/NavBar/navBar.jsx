7e3fe94e237fe2a3234c6b12e880f5b482d18810
Simon Milord <simon.milord@gmail.com>
2022-03-24 00:54:12 -0400

    fixed display of navbar

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -8,17 +8,23 @@ import SearchIcon from '@mui/icons-material/Search';
 export default function NavBar(props) {
   return (
     <div className='navBar'>
-      <div className='logo'>
-        <Link to="/" className='logo__link'><img className='logo__img' src={Logo} alt="stonkers logo"></img></Link>
-        <h1 className='logo__title'>Stonkers</h1>
+      <div className='nav'>
+        <div className='nav__left'>
+          <Link to="/" className='nav__logo'><img className='nav__img' src={Logo} alt="stonkers logo"></img></Link>
+          <Link to="/" className="nav__link nav__link--home">Home</Link>
+          <Link to="/watchlist" className="nav__link nav__link--watchlist" >Watchlist</Link>
+        </div>
+        <Link to="/login" className="nav__link nav__link--signout" >Sign out</Link>
       </div>
-      <div className='search'>
-        <SearchIcon color="action"/>
-        <input className="search__input" type="text" name="" placeholder='Search...' />
-      </div>
-      <div className='btns'>
-        <Link to="/watchlist" className="btns__link btns__link--watchlist" >Watchlist</Link>
-        <Link to="/login" className="btns__link btns__link--logout">Sign out</Link>
+      <div className='navBar-bottom'>
+        <div className='search'>
+          <SearchIcon color="action"/>
+          <input className="search__input" type="text" name="" placeholder='Search...' />
+        </div>
+        <div className='btns'>
+          <Link to="/" className="btns__link btns__link--home">Home</Link>
+          <Link to="/watchlist" className="btns__link btns__link--watchlist" >Watchlist</Link>
+        </div>
       </div>
     </div>
   );
