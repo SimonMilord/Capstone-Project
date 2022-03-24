d10e5afef52a8dec299c6ad2f330e9043a26d28b
Simon Milord <simon.milord@gmail.com>
2022-03-24 10:10:30 -0400

    added JSX structure and basic styling for quote component

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './navBar.scss';
-import { Link} from "react-router-dom";
+import { Link, NavLink} from "react-router-dom";
 import Logo from "../../assets/Logo/stonkers-logo.png";
 import SearchIcon from '@mui/icons-material/Search';
 
@@ -10,20 +10,20 @@ export default function NavBar(props) {
     <div className='navBar'>
       <div className='nav'>
         <div className='nav__left'>
-          <Link to="/" className='nav__logo'><img className='nav__img' src={Logo} alt="stonkers logo"></img></Link>
-          <Link to="/" className="nav__link nav__link--home">Home</Link>
-          <Link to="/watchlist" className="nav__link nav__link--watchlist" >Watchlist</Link>
+          <NavLink to="/" className='nav__logo'><img className='nav__img' src={Logo} alt="stonkers logo"></img></NavLink>
+          <NavLink to="/" className="nav__link nav__link--home">Home</NavLink>
+          <NavLink to="/watchlist" className="nav__link nav__link--watchlist" >Watchlist</NavLink>
         </div>
-        <Link to="/login" className="nav__link nav__link--signout" >Sign out</Link>
+        <NavLink to="/login" className="nav__link nav__link--signout" >Sign out</NavLink>
       </div>
       <div className='navBar-bottom'>
         <div className='search'>
           <SearchIcon color="action"/>
-          <input className="search__input" type="text" name="" placeholder='Search...' />
+          <input className="search__input" type="text" name="" placeholder='Search for stocks...' />
         </div>
         <div className='btns'>
-          <Link to="/" className="btns__link btns__link--home">Home</Link>
-          <Link to="/watchlist" className="btns__link btns__link--watchlist" >Watchlist</Link>
+          <NavLink to="/" className="btns__link btns__link--home">Home</NavLink>
+          <NavLink to="/watchlist" className="btns__link btns__link--watchlist" >Watchlist</NavLink>
         </div>
       </div>
     </div>
--- client/src/components/Quote/quote.jsx
+++ client/src/components/Quote/quote.jsx
@@ -4,7 +4,20 @@ import './quote.scss';
 export default function Quote(props) {
   return (
     <div className='quote'>
-
+      <div className='quote__top'>
+        <div className='quote__company'>
+          <div className='quote__ticker'>AAPL - <span className='quote__exchange'>NYSE</span></div>
+          <div className='quote__name'>Apple Inc.</div>
+        </div>
+        <div className='quote__btn'>
+          <button className='quote__addBtn' type="submit">Add to watchlist</button>
+        </div>
+      </div>
+      <div className='quote__bottom'>
+        <p className='quote__price'>$158.89$</p>
+        <p className='quote__var quote__var--perc'>8.91%</p>
+        <p className='quote__var quote__var--doll'>+10.27</p>
+      </div>
     </div>
   );
 }
