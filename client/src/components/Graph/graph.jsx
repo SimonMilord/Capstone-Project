24f7247ab833852383e619151f58fa1f4c69303f
Simon Milord <simon.milord@gmail.com>
2022-03-25 00:05:38 -0400

    added quote styling

--- client/src/components/Graph/graph.jsx
+++ client/src/components/Graph/graph.jsx
@@ -4,7 +4,11 @@ import './graph.scss';
 export default function Graph(props) {
   return (
     <div className='graph'>
+      <div className='container'>
+        <canvas className='graph' id='graph'>
 
+        </canvas>
+      </div>
     </div>
   );
 }
