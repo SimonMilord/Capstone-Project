e1e0710ebd85a43327a93b15e7d19abaf6d2108c
Simon Milord <simon.milord@gmail.com>
2022-03-27 17:13:56 -0400

    added color red and green depending on the price of the quote

--- client/src/components/Quote/quote.jsx
+++ client/src/components/Quote/quote.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import './quote.scss';
+import React from "react";
+import "./quote.scss";
 
 export default function Quote(props) {
   return (
-    <div className='quote'>
-      <div className='quote__top'>
-        <div className='quote__company'>
-          <div className='quote__ticker'>{props.symbol} - <span className='quote__exchange'>{props.profile.exchange}</span></div>
-          <div className='quote__name'>{props.name}</div>
+    <div className="quote">
+      <div className="quote__top">
+        <div className="quote__company">
+          <div className="quote__ticker">
+            {props.symbol} -{" "}
+            <span className="quote__exchange">{props.profile.exchange}</span>
+          </div>
+          <div className="quote__name">{props.name}</div>
         </div>
-        <div className='quote__btn'>
-          <button className='quote__addBtn' type="submit">Add to watchlist</button>
+        <div className="quote__btn">
+          <button className="quote__addBtn" type="submit">
+            Add to watchlist
+          </button>
         </div>
       </div>
-      <div className='quote__bottom'>
-        <p className='quote__price'>${props.quote.c} (USD)</p>
-        <p className='quote__var quote__var--perc'>{Number(props.quote.dp).toFixed(2)}%</p>
-        <p className='quote__var quote__var--doll'>{Number(props.quote.d).toFixed(2)}</p>
+      <div className="quote__bottom">
+        <p className="quote__price">${props.quote.c} (USD)</p>
+        <p className={props.quote.dp > 0 ? "quote__var" : "quote__var--down"}>
+          {Number(props.quote.dp).toFixed(2)}%
+        </p>
+        <p className={props.quote.dp > 0 ? "quote__var" : "quote__var--down"}>
+          {Number(props.quote.d).toFixed(2)}
+        </p>
       </div>
     </div>
   );
