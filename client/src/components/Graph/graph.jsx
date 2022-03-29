e126cb8ccfa8fdf01cd24b1581bccd7e6bb2a673
Simon Milord <simon.milord@gmail.com>
2022-03-28 20:51:25 -0400

    made a graph boilerplate appear

--- client/src/components/Graph/graph.jsx
+++ client/src/components/Graph/graph.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './graph.scss';
+// import 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
 
 export default function Graph(props) {
 
@@ -9,10 +12,27 @@ export default function Graph(props) {
 
   // console.log(pricesArray);
   // console.log(timeArray);
-
+  const data = [1];
   return (
     <div className='graph'>
-
+      <Line
+        datasetIdKey='id'
+        data={{
+          labels: ['Jun', 'Jul', 'Aug'],
+          datasets: [
+            {
+              id: 1,
+              label: '',
+              data: [],
+            },
+            {
+              id: 2,
+              label: '',
+              data: [],
+            },
+          ],
+        }}
+      />
     </div>
   );
 }
