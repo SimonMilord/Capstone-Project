5f527b7c9b90756f838a04ede0dfe7f9feb0d567
Simon Milord <simon.milord@gmail.com>
2022-04-05 22:19:09 -0400

    pre deploy

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const usersRoutes = require("./routes/usersRoutes");
+const User = require("./models/user");
+const { db } = require("./models/user");
+const app = express();
+const jwt = require("jsonwebtoken");
+const PORT = process.env.PORT || 8080;
+app.use(cors());
+
+// put in env password
+mongoose
+  .connect("mongodb+srv://stonkers_user:ZJJzJZhoO1EPLCxm@stonkers.9ivjh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  )
+  .then();
+// middleware
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/user", usersRoutes);
+
+app.listen(PORT, () => {
+  console.log(`🚀Listening on port: ${PORT}`);
+});
